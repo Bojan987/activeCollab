9013f20a5e2f86b955818a54b2776830f5a05c92
Bojan Majmunovic <bojan.majmunovic@telenor.rs>
2023-01-23 09:49:00 +0100

    fix- issue with home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 //mockup data
 import { data } from "../../../data/data";
 import Row from "../../components/Rows/Row";
-import "./Homepage.css";
 
 const HomePage = () => {
   return (
